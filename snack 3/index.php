5248080584669be06b4955cbe0e52b516de74c06
kritoma <toma.kristian97@gmail.com>
2022-06-23 12:14:45 +0200

    snack 3

---
+++ snack 3/index.php
@@ -0,0 +1,65 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Posts</h1>
+    <?php foreach($posts as $key => $post) { ?>
+        <h2><?= $key; ?></h2>
+        <?php foreach($post as $titles) { ?>
+        <ul>
+            <?php foreach($titles as $keys => $title) { ?>
+                <li><?= "$keys : $title" ?></li>
+            <?php } ?>
+        </ul>
+        <?php } ?>
+    <?php } ?>
+</body>
+</html>
